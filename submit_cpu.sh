baab2c761b19e096016245e714cd8cf53229c2ea
PythonFZ <fzills@icp.uni-stuttgart.de>
2023-09-23 18:57:46 +0200

    update DFT parameters; ZnTrack v0.7.0

--- submit_cpu.sh
+++ submit_cpu.sh
@@ -2,12 +2,13 @@
 #SBATCH --partition=cpu-multi
 #SBATCH --ntasks=8
 #SBATCH --cpus-per-task=8
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mem=16gb
 
 source ~/.bashrc
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export IPSUITE_CP2K_SHELL="mpirun -n ${SLURM_NTASKS} cp2k_shell.psmp"
 
 module load compiler/gnu
 module load mpi/openmpi
