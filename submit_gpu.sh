63ac19aa03f9b69a6706ffa49d4e3250b2773729
PythonFZ <fzills@icp.uni-stuttgart.de>
2023-09-26 12:30:01 +0200

    run AL0..4

--- submit_gpu.sh
+++ submit_gpu.sh
@@ -9,6 +9,7 @@
 source ~/.bashrc
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export IPSUITE_CP2K_SHELL="mpirun -n ${SLURM_NTASKS} cp2k_shell.psmp"
 
 module load compiler/gnu
 module load mpi/openmpi
--- submit_single_multi.sh
+++ submit_single_multi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=single
 #SBATCH --ntasks=8
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --time=120:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32gb
@@ -9,6 +9,7 @@
 source ~/.bashrc
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export IPSUITE_CP2K_SHELL="mpirun -n ${SLURM_NTASKS} cp2k_shell.psmp"
 
 module load compiler/gnu
 module load mpi/openmpi
