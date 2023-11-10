7c3d9c66b8870aa79084e28b1180dfe6b449b384
PythonFZ <fabian.zills@web.de>
2023-11-10 07:32:43 +0100

    add DFT stage

--- main.py
+++ main.py
@@ -227,13 +227,13 @@ with project.group("ML4"):
         min_distance=50,
     )
 
-    # cp2k = ips.calculators.CP2KSinglePoint(
-    #     data=selection.atoms,
-    #     cp2k_params="config/cp2k.yaml",
-    #     cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS", "dftd3.dat"],
-    # )
+    cp2k = ips.calculators.CP2KSinglePoint(
+        data=selection.atoms,
+        cp2k_params="config/cp2k.yaml",
+        cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS", "dftd3.dat"],
+    )
 
-    # train_data += cp2k.atoms
+    train_data += cp2k.atoms
 
 project.build()
 
