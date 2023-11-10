fd80faea5f20ff30da936890944bce1016a05b7b
PythonFZ <fabian.zills@web.de>
2023-11-10 17:51:14 +0100

    ML0

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ with project.group("classical"):
         cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS", "dftd3.dat"],
     )
 
-    filtered = ips.configuration_selection.FilterOutlier(
-        data=cp2k.atoms
-    )
+    filtered = ips.configuration_selection.FilterOutlier(data=cp2k.atoms)
 
     train_data = ips.configuration_selection.RandomSelection(
         data=filtered.atoms, n_configurations=1000
@@ -32,31 +30,50 @@ with project.group("classical"):
 
     test_data = validation_data.excluded_atoms
 
+    train_data = train_data.atoms
+
     model = ips.models.Apax(
-        data=train_data.atoms,
+        data=train_data,
         validation_data=validation_data.atoms,
         config="config/initial_model.yaml",
     )
 
-    prediction = ips.analysis.Prediction(
-        data=test_data, model=model
-    )
-    metrics = ips.analysis.PredictionMetrics(
-        data=prediction
-    )
+    prediction = ips.analysis.Prediction(data=test_data, model=model)
+    metrics = ips.analysis.PredictionMetrics(data=prediction)
 
 with project.group("ML0"):
-     md = ips.calculators.ASEMD(
+    md = ips.calculators.ASEMD(
         data=data.atoms,
         data_id=-1,
         model=model,
         thermostat=thermostat,
         checker_list=[uncertainty_check],
         steps=50000,
-        sampling_rate=1,
+        sampling_rate=10,
     )
 
+    geo_opt = ips.calculators.ASEGeoOpt(
+        model=model,
+        data=md.atoms,
+        data_id=-1,
+        optimizer="FIRE",
+        run_kwargs={"fmax": 0.01},
+    )
 
+    kernel_selection = ips.models.apax.BatchKernelSelection(
+        data=md.atoms + geo_opt.atoms,
+        train_data=train_data,
+        models=model,
+        n_configurations=50,
+        processing_batch_size=4,
+    )
+
+    cp2k = ips.calculators.CP2KSinglePoint(
+        data=kernel_selection.atoms,
+        cp2k_params="config/cp2k.yaml",
+        cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS", "dftd3.dat"],
+    )
+
+    train_data += cp2k.atoms
 
 project.build()
-
