a0314d940463a303949e7b1c963d602a5ad748d4
PythonFZ <fabian.zills@web.de>
2023-11-15 09:38:10 +0100

    ML13

--- main.py
+++ main.py
@@ -442,5 +442,32 @@ with project.group("ML13") as grp:
         sampling_rate=10,
     )
 
+    kernel_selection = ips.models.apax.BatchKernelSelection(
+        data=md.atoms,
+        train_data=train_data,
+        models=model,
+        n_configurations=50,
+        processing_batch_size=4,
+    )
+
+    # cp2k = ips.calculators.CP2KSinglePoint(
+    #     data=kernel_selection.atoms,
+    #     cp2k_params="config/cp2k.yaml",
+    #     cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS", "dftd3.dat"],
+    # )
+
+    # train_data += cp2k.atoms
+
+    # model = ips.models.Apax(
+    #     data=train_data,
+    #     validation_data=validation_data.atoms,
+    #     config="config/initial_model.yaml",
+    # )
+
+    # prediction = ips.analysis.Prediction(data=test_data, model=model)
+    # metrics = ips.analysis.PredictionMetrics(data=prediction)
+    # ips.analysis.EnergyHistogram(data=train_data, bins=100)
+    # ips.analysis.ForcesHistogram(data=train_data)
+
 
 project.build(nodes=[grp])
