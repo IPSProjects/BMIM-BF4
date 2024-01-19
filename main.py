6331ef34217029c20816325e12fb3cf916e16146
PythonFZ <fabian.zills@web.de>
2024-01-19 13:27:57 +0100

    353K, 16 pairs, preparation

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import ipsuite as ips 
+import ipsuite as ips
 import zntrack
 
 project = ips.Project(automatic_node_names=True)
 
-ramp_density = ips.calculators.RescaleBoxModifier(
-    density=1162
-)
+ramp_density = ips.calculators.RescaleBoxModifier(density=1162)
 thermostat = ips.calculators.LangevinThermostat(
     temperature=353, friction=0.01, time_step=0.5
 )
 
-mlp = zntrack.from_rev("ML17_training_MLModel_2", rev="ml17", remote="https://github.com/IPSProjects/BMIM-BF4_new.git")
+mlp = zntrack.from_rev(
+    "ML17_training_MLModel_2",
+    rev="ml17",
+    remote="https://github.com/IPSProjects/BMIM-BF4_new.git",
+)
 
-with project.group("depl") as depl:
-    anion = ips.configuration_generation.SmilesToAtoms(
-        smiles="[B-](F)(F)(F)F"
-    )
-    cation = ips.configuration_generation.SmilesToAtoms(
-        smiles="CCCCN1C=C[N+](=C1)C"
-    )
+with project.group("setup"):
+    anion = ips.configuration_generation.SmilesToAtoms(smiles="[B-](F)(F)(F)F")
+    cation = ips.configuration_generation.SmilesToAtoms(smiles="CCCCN1C=C[N+](=C1)C")
 
     single_structure = ips.configuration_generation.Packmol(
         data=[cation.atoms, anion.atoms],
@@ -29,7 +27,7 @@ with project.group("depl") as depl:
 
     structure = ips.configuration_generation.Packmol(
         data=[single_structure.atoms],
-        count=[16], # 16, 32, 64
+        count=[16],  # 16, 32, 64
         density=900,
     )
 
@@ -51,7 +49,7 @@ with project.group("depl") as depl:
         sampling_rate=10,
     )
 
-    # relaxation 
+    # relaxation
 
     md = ips.calculators.ASEMD(
         data=density_md.atoms,
@@ -62,4 +60,20 @@ with project.group("depl") as depl:
         sampling_rate=10,
     )
 
-project.build(nodes=[depl])
+with project.group("AIMD"):
+    model = ips.calculators.CP2KSinglePoint(
+        data=cation.atoms,  # we still need some input here
+        cp2k_params="config/cp2k.yaml",
+        cp2k_files=["GTH_BASIS_SETS", "GTH_POTENTIALS"],
+    )
+
+    md = ips.calculators.ASEMD(
+        data=md.atoms,
+        data_id=-1,
+        model=model,
+        thermostat=thermostat,
+        steps=20_000,
+        sampling_rate=1,
+    )
+
+project.build()
