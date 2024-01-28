dcecb98e7aa8cafc68091e0d82f6643885b11605
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-01-28 10:46:19 +0100

    353K, 32 pairs, 20 ns

---
+++ submit_ehlers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64gb
+#SBATCH --job-name=353K-32Pairs
+
+source ~/.bashrc
+conda activate ips
+
+# dvc repro
+zntrack run ipsuite.nodes.ApaxJaxMD --name depl_ApaxJaxMD
