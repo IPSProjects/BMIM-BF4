3f36ee5b255d6432eb9827614655feba2fa7962e
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-01-30 12:10:39 +0100

    293K, 8 pairs, 20 ns

--- submit_ehlers.sh
+++ submit_ehlers.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=64gb
-#SBATCH --job-name=293K-32Pairs
+#SBATCH --mem=32gb
+#SBATCH --job-name=293K-8Pairs
 
 source ~/.bashrc
 conda activate ips
