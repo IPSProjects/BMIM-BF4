fd0242bb6b92456ee0ea3e4901de37d9adb7a6b4
PythonFZ <fabian.zills@web.de>
2024-02-02 07:50:57 +0100

    500K, 16 pairs, 2 ns

--- main.py
+++ main.py
@@ -935,10 +935,10 @@ with project.group("ML17_eval") as ml17_eval:
 
 
 ramp_density = ips.calculators.RescaleBoxModifier(
-    density=1162
+    density=1059
 )
 thermostat = ips.calculators.LangevinThermostat(
-    temperature=353, friction=0.01, time_step=0.5
+    temperature=500, friction=0.01, time_step=0.5
 )
 # https://pubs.acs.org/doi/10.1021/jp306146s
 
@@ -959,7 +959,7 @@ with project.group("depl") as depl:
 
     structure = ips.configuration_generation.Packmol(
         data=[single_structure.atoms],
-        count=[8], # 16, 32, 64
+        count=[16], # 16, 32, 64
         density=900,
     )
 
